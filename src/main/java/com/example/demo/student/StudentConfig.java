55fd8d74a51b3ef4116ef6f5a444c3c85651158b
Oratile Masipa <masipaoratile@gmail.com>
2023-07-17 12:55:22 +0200

    adding all files

---
+++ src/main/java/com/example/demo/student/StudentConfig.java
@@ -0,0 +1,39 @@
+package com.example.demo.student;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@Configuration
+public class StudentConfig {
+
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository repository) {
+        return args -> {
+
+               Student mariam = new Student(
+                            1L,
+                            "Mariam",
+                            "mariam@gmail.com",
+                            LocalDate.of(2000, Month.JANUARY, 5)
+
+                    );
+
+            Student john = new Student(
+                    "John",
+                    "johndoe@gmail.com",
+                    LocalDate.of(2003, Month.JANUARY, 5)
+
+            );
+
+            repository.saveAll(
+                    List.of(mariam, john)
+            );
+
+        };
+    }
+}
